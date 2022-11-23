6ce3b9ee27f9804bb8a983bd964a22d394f5a186
yoshida874 <ud8mnzdd@gmail.com>
2022-11-23 23:55:31 +0900

    devとprodに分け

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { merge } = require("webpack-merge");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  plugins: [new BundleAnalyzerPlugin()],
+});
