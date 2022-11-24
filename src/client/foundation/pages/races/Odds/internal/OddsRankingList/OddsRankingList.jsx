f0786c894e453cc5274d15eb67eae194c916ca5d
yoshida874 <ud8mnzdd@gmail.com>
2022-11-25 02:56:41 +0900

    lodashを使用

--- src/client/foundation/pages/races/Odds/internal/OddsRankingList/OddsRankingList.jsx
+++ src/client/foundation/pages/races/Odds/internal/OddsRankingList/OddsRankingList.jsx
@@ -1,7 +1,8 @@
+import { sortBy } from "lodash/sortBy";
+import { take } from "lodash/take";
 import React from "react";
 import styled from "styled-components";
 
-import { sortBy, take } from "../../../../../../../../scripts/common";
 import { BaseButton } from "../../../../../components/buttons/BaseButton";
 import { EntryCombination } from "../../../../../components/displays/EntryCombination";
 import { Stack } from "../../../../../components/layouts/Stack";
@@ -66,7 +67,10 @@ const RankNo = styled.div`
 
 /** @type {React.VFC<Props>} */
 export const OddsRankingList = ({ isRaceClosed, odds, onClickOdds }) => {
-  const sortedOdds = take(sortBy(odds), 50);
+  const sortedOdds = take(
+    sortBy(odds, (item) => item.odds),
+    50,
+  );
 
   return (
     <Wrapper>
