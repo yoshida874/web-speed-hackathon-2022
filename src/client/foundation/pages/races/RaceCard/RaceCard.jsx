81329f8bd7c8c9d2f0e8d97685cbf7f91d75ddb6
yoshida874 <ud8mnzdd@gmail.com>
2022-11-25 01:19:03 +0900

    400×225のavifを作成

--- src/client/foundation/pages/races/RaceCard/RaceCard.jsx
+++ src/client/foundation/pages/races/RaceCard/RaceCard.jsx
@@ -47,7 +47,12 @@ export const RaceCard = () => {
       <Section dark shrink>
         <LiveBadge>Live</LiveBadge>
         <Spacer mt={Space * 2} />
-        <TrimmedImage height={225} src={data.image} width={400} />
+        <img
+          height={225}
+          loading="lazy"
+          src={`${data.image.slice(0, -4)}.avif`}
+          width={400}
+        ></img>
       </Section>
 
       <Spacer mt={Space * 2} />
